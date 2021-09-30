d69c2c9fb3757062c86afdd974cb3076a0ebb7ec
sergio.segaty <sergio.segaty@gmail.com>
2021-09-30 17:02:57 -0300

    Finished EsLint+Prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,57 +1,29 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    plugins: ['react', '@typescript-eslint', 'prettier'],
-    extends: [
-        'plugin:react/recommended',
-        'airbnb',
-        'prettier',
-        'plugin:prettier/recommended',
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-use-before-define': 'off',
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': ['error'],
+    'import/prefer-default-export': 'off',
+    'react/prop-types': 'off',
+    'react/jsx-filename-extension': [
+      1,
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
     ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
-        ecmaVersion: 12,
-        sourceType: 'module',
-    },
-    rules: {
-        'prettier/prettier': 'error',
-        'no-use-before-define': 'off',
-        '@typescript-eslint/no-use-before-define': ['error'],
-        'react/jsx-filename-extension': [
-            'warn',
-            {
-                extensions: ['.tsx'],
-            },
-        ],
-        'import/extensions': [
-            'error',
-            'ignorePackages',
-            {
-                ts: 'never',
-                tsx: 'never',
-            },
-        ],
-        'no-shadow': 'off',
-        '@typescript-eslint/no-shadow': ['error'],
-        '@typescript-eslint/explicit-function-return-type': [
-            'error',
-            {
-                allowExpressions: true,
-            },
-        ],
-        'max-len': [
-            'warn',
-            {
-                code: 80,
-            },
-        ],
-        'import/prefer-default-export': 'off',
-        'react/prop-types': 'off',
-        '@typescript-eslint/explicit-function-return-type': false,
-    },
-}
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'prettier/prettier': 'error',
+  },
+};
