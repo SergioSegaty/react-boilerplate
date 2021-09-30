a630977063ae533edb9a9db7d5fa3052dff5e524
sergio.segaty <sergio.segaty@gmail.com>
2021-09-30 16:47:04 -0300

    Added Prettier, configured wtih EsLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,57 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    env: {
+        browser: true,
+        es2021: true,
     },
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-  ],
-  rules: {
-  },
-};
+    plugins: ['react', '@typescript-eslint', 'prettier'],
+    extends: [
+        'plugin:react/recommended',
+        'airbnb',
+        'prettier',
+        'plugin:prettier/recommended',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    rules: {
+        'prettier/prettier': 'error',
+        'no-use-before-define': 'off',
+        '@typescript-eslint/no-use-before-define': ['error'],
+        'react/jsx-filename-extension': [
+            'warn',
+            {
+                extensions: ['.tsx'],
+            },
+        ],
+        'import/extensions': [
+            'error',
+            'ignorePackages',
+            {
+                ts: 'never',
+                tsx: 'never',
+            },
+        ],
+        'no-shadow': 'off',
+        '@typescript-eslint/no-shadow': ['error'],
+        '@typescript-eslint/explicit-function-return-type': [
+            'error',
+            {
+                allowExpressions: true,
+            },
+        ],
+        'max-len': [
+            'warn',
+            {
+                code: 80,
+            },
+        ],
+        'import/prefer-default-export': 'off',
+        'react/prop-types': 'off',
+        '@typescript-eslint/explicit-function-return-type': false,
+    },
+}
